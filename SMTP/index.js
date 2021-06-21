68d4f138752cb3def59a0335ee9b7d0742c830e3
ridoy789 <85821834+ridoy789@users.noreply.github.com>
2021-06-21 17:14:17 +0600

    Add files via upload

---
+++ SMTP/index.js
@@ -0,0 +1,16 @@
+﻿//index.js  
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "pliclmushfiq@gmail.com",
+	Password : "Ridoy456",
+	To : 'ridoy123@gmail.com',
+	From : "<sender’s email address>",
+	Subject : "<email subject>",
+	Body : "<email body>",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
+
+	
